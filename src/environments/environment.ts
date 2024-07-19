9624421ae92fa07d6ad1ed5aabc30337db8a412a
Abimbola <ahassan@chapelhilldenham.com>
2024-07-19 20:33:06 +0100

    Correct endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,5 @@
 export const environment = {
-  baseApiUrl: 'https://pov2-prod.azurewebsites.net/api/v2',
-  FUND_BASES: ['https://dollarfund-2.azurewebsites.net'],
-  ZANIBAL_BASES: [
-    'https://appserver.chapelhilldenham.com',
-    'https://mds.zanibal.com',
-  ],
+  baseApiUrl: 'https://po-chd.zanibal.com/api/v2',
 
   SECRET_KEY: 'NjIwMjA0NTcxfQ.27VYXVdIMvIbQFJFZ',
   IV_KEY: '5Bl83ks5O9XN80Kj',
